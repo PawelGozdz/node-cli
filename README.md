4265fc7ae76a19414fc56647d627d70dc574089d
PawelGozdz <pawelgozdz83@gmail.com>
2024-03-15 16:38:38 +0100

    feature: demo

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # node-cli
 Node CLI small projects
+
+> Demo description
+
+## Usage
+To sue this CLI run following command:
+
+```sh
+npx pg_demo
+```
--- first-app/index.js
+++ first-app/index.js
@@ -1,15 +1,19 @@
 #!/usr/bin/env node
 
-const clearConsole = require('clear-any-console')
 const pkgJSON = require('../package.json');
+const welcome = require('cli-welcome');
 
-clearConsole();
 
-console.log(`
-NAME:         ${pkgJSON.name}
-VERSION:      ${pkgJSON.version}
-DESCRIPTION:  ${pkgJSON.description}
-`)
+welcome({
+  title: pkgJSON.name,
+  tagLine: `Welcome to ${pkgJSON.name}`,
+  version: pkgJSON.version,
+  description: pkgJSON.description,
+  bgColor: `#6937FF`,
+  color: `#000000`,
+  bold: true,
+  clear: true
+})
 
 console.log(`
 pg_programming
