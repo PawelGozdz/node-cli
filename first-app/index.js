3328f777542fc0e7fd5f03306f2a8d9ee0aa833d
PawelGozdz <pawelgozdz83@gmail.com>
2024-03-15 16:16:59 +0100

    feature: first commit

---
+++ first-app/index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const clearConsole = require('clear-any-console')
+
+clearConsole();
+
+console.log(`
+pg_programming
+
+Dev
+
+X:      https://x.com
+github: https://github.com
+
+`) 
